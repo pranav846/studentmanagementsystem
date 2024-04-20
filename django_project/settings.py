5731365727547ff96d99d851b45ad99cf6e26be5
EC2 Default User <ec2-user@ip-172-31-41-79.eu-west-1.compute.internal>
2024-04-20 21:01:20 +0000

    csss

--- django_project/settings.py
+++ django_project/settings.py
@@ -129,7 +129,7 @@ USE_I18N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-TATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
